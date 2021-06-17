ddb93ff4df6c1fe97b321bf30f9e2263bced16b4
Michel <michel.mef@gmail.com>
2021-06-16 20:09:03 -0400

    desativando função de apagar texto

--- js/portjs.js
+++ js/portjs.js
@@ -1,7 +1,7 @@
 //const toggle = document.getElementById('toggle');
 const dtAtual = document.getElementById('dataAtual');
 let data = new Date();
-function toggle(){
+function toggle() {
     var header = document.getElementById("header");
     header.classList.toggle('active')
 }
@@ -21,3 +21,31 @@ $(document).ready(function () {
     };
     loader();
 });
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    var texto = '';
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+        texto += letra;
+    });
+
+}
+//função desativada 
+function limpar(elemento) {
+    var char = elemento.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(() => {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return;
+        }
+        elemento.innerHTML = char.slice(0, nr);
+    }, 75);
+
+}
+
+const subtitulo = document.querySelector('#home .lead');
+typeWriter(subtitulo);
+//limpar(subtitulo);
